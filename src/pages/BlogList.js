a6f276d7b3642c2798e6ba1684c980a608277b51
Mhd Yooshah <yooshahkk@gmail.com>
2023-08-20 17:12:24 +0530

    page header

---
+++ src/pages/BlogList.js
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import BlogListHeader from "../components/Header";
+
+export const BlogList = ({}) => {
+  return (
+    <Container fixed>
+      <BlogListHeader />
+    </Container>
+  );
+};
