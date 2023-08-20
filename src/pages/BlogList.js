a4ceac40cee84061a53b37a779586736ff63e55e
Mhd Yooshah <yooshahkk@gmail.com>
2023-08-20 17:49:15 +0530

    blogs get api implemeted

--- src/pages/BlogList.js
+++ src/pages/BlogList.js
@@ -10,6 +10,14 @@ import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
 
 export const BlogList = ({}) => {
+  const [blogs, setBlogs] = React.useState([]);
+  const [searchTitle, setSearchTitle] = React.useState("");
+  const fetchBlogs = async (title, page) => {
+    const response = await axios.get(
+      `https://root-blog.prismic.io/api/v2/documents/search?ref=ZGS6jxEAACUAsSPz&q=%5B%5Bfulltext(my.post.title%2C+%22${title}%22)%5D%5D&q=%5B%5Bat(document.type%2C+%22post%22)%5D%5D&page=${page}&pageSize=10`
+    );
+    setBlogs(response.data)
+  };
   return (
     <>
       <Container fixed sx={{ display: "flex", flexWrap: "wrap" }}>
@@ -29,8 +37,15 @@ export const BlogList = ({}) => {
             sx={{ ml: 1, flex: 1 }}
             placeholder="Search blogs here"
             inputProps={{ "aria-label": "search Blog" }}
+            onChange={(e) => setSearchTitle(e.target.value)}
+            value={searchTitle}
           />
-          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <IconButton
+            type="button"
+            sx={{ p: "10px" }}
+            aria-label="search"
+            onClick={() => fetchBlogs(searchTitle, 1)}
+          >
             <SearchIcon />
           </IconButton>
         </Paper>
